3298a3a704737cea84ddcb0d20654af9c162df50
dim-sen <dimas.10119285@mahasiswa.unikom.ac.id>
2023-06-03 16:27:34 +0700

    First Commit

---
+++ src/main/java/com/online/pasaronlineapp/PasarOnlineAppApplication.java
@@ -0,0 +1,13 @@
+package com.online.pasaronlineapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasarOnlineAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasarOnlineAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/online/pasaronlineapp/PasarOnlineAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.online.pasaronlineapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasarOnlineAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
